9574c4cd7e8c73358b643257da134f98b51d99e4
Sydney Werbach <sydwer@gmail.com>
2019-09-30 02:06:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+
+def map(source_array) #use basic name dont be fun it aint work
+  new_and_improved_array = []
+  i = 0 
+  while i < source_array.length 
+  new_and_improved_array.push(yield(source_array[i]))
+  i+= 1
+end
+return new_and_improved_array
+end 
+
+def reduce(source_array, starting_value = nil) 
+ if starting_value
+   launch_point = starting_value
+   i = 0
+ else 
+   launch_point = source_array[0]
+   i = 1
+ end
+ while i < source_array.length do 
+   launch_point = yield(launch_point, source_array[i])
+   i += 1
+end
+return launch_point 
+end
+
